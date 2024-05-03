1d1853d43e442dd7a90130bf8f292a2b672ce28d
Janavi Chadha <48561914+JanaviChadha@users.noreply.github.com>
2024-05-03 02:16:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import turtle
+"""
+Do not edit above this line 
+
+NOTE: once you finish a challenge, we recommend commenting out the code from that challenge before starting the next one; otherwise, repl.it will draw all shapes over each other, and it will be messy. To accomplish this, highlight that code from that challenge and then press ctrl + /, or if you're on a mac, then cmd + /. Alternatively, you can comment out the lines one by one by placing a # before each line
+"""
+
+#declare your turtle under this line 
+
+"""
+Draw a line 
+"""
+
+#place code for drawing line here 
+
+"""
+Draw a square 
+"""
+
+#place code for drawing square here 
+
+"""
+Draw an equilateral triangle
+"""
+
+#place code for drawing an equilateral triangle here 
+
+"""
+Draw a non-equilateral triangle
+"""
+
+#place code for drawing a non-equilateral triangle here 
+
+"""
+Draw a star 
+"""
+
+#place code for drawing a star here 
+
+"""
+Draw a self portrial  
+"""
+
+#place code for drawing a self portrait here 
+
+
+
+
+
